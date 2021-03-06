5c94fa9c9d1ea597ae0d3cd81fc18d62b3c33d59
Fabian Brenes <fabianbrenes@ice.co.cr>
2021-03-06 02:51:52 +0000

    Parte Grafica -2

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="main">
+			<div className="secundario">
+				<h1 className="titulo">toDoList</h1>
+				<div className="listadoVacio">
+					<ul class="list-group">
+						<li className="list-group-item">
+							<input type="text" />
+						</li>
+						<li className="list-group-item">
+							Dapibus ac facilisis in
+						</li>
+						<li className="list-group-item">Morbi leo risus</li>
+						<li className="list-group-item">
+							Porta ac consectetur ac
+						</li>
+						<li className="list-group-item">Vestibulum at eros</li>
+					</ul>
+				</div>
+				<div className="listadoIniciado">
+					<p>Tareas Pendientes</p>
+				</div>
+			</div>
 		</div>
 	);
 }
