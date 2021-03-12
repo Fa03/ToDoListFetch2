d0d4be3e8ca79e27426ee7b9badf9d7228f3f17f
Fabian Brenes <fabianbrenes@ice.co.cr>
2021-03-12 19:34:48 +0000

    Final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,20 +1,43 @@
 import React, { useState } from "react";
 import numeroTareasPie, { cantidadTareas } from "./numeroTareasPie";
-import listIncrement from "./listIncrement";
 
 export function Home() {
 	let [tarea, setTarea] = useState([]);
 
 	let agregarTarea = (evento, teclaPresionada) => {
 		if (teclaPresionada === 13) {
-			setTarea(tarea.concat(evento.target.value)); // .push no funciona en REACT genera el lenght no el dato
-			evento.target.value = ""; //limpia el valor del textarea despues del ENTER
+			if (evento.target.value == "") {
+				alert("Task space is empty");
+			} else {
+				setTarea(tarea.concat(evento.target.value)); // .push no funciona en REACT genera el lenght no el dato
 
-			// console.log("si Entro");
+				evento.target.value = ""; //limpia el valor del textarea despues del ENTER
+			}
 		}
+	};
 
-		// console.log(tarea);
-		// console.log(tarea);
+	let listIncrement = () => {
+		return (
+			<li className="list-group-item">
+				{tarea.map((t, index) => {
+					return (
+						<li className="list-group-item" key={index}>
+							{t}
+							<button
+								className="button"
+								onClick={e => eliminarTarea(index)}>
+								&#10003;
+							</button>
+						</li>
+					);
+				})}
+			</li>
+		);
+	};
+
+	const eliminarTarea = indice => {
+		let newTarea = [...tarea.slice(0, indice), ...tarea.slice(indice + 1)];
+		setTarea(newTarea);
 	};
 
 	return (
@@ -31,9 +54,7 @@ export function Home() {
 								onKeyPress={e => agregarTarea(e, e.charCode)}
 							/>
 						</li>
-						<li className="list-group-item">
-							{listIncrement(tarea)}
-						</li>
+						<li className="list-group-item">{listIncrement()}</li>
 					</ul>
 				</div>
 				<div className="listadoIniciado">
